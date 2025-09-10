6f0c7dade555ac93f2fc991323ab456132dbe309
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-09-10 21:11:00 +0100

    add the snapshot repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,20 @@ pluginManagement {
         id(quarkusPluginId) version quarkusPluginVersion
     }
 }
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        /*
+        add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future when this
+        will not be necessary when this library is released as a non-SNAPSHOT version.
+         */
+        maven {
+            url= uri("https://central.sonatype.com/repository/maven-snapshots/")
+        }
+    }
+}
+
 rootProject.name="tap-server"
 
 includeBuild("../ivoacore") //TODO separate this build when publishing
