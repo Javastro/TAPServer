b47d2c0e4fb4b8786aa9e363fc99d29033409592
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-04-08 09:57:46 +0100

    remove annotation

--- src/main/java/org/javastro/ivoa/tap/BaseTAPResource.java
+++ src/main/java/org/javastro/ivoa/tap/BaseTAPResource.java
@@ -10,10 +10,8 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
-import org.javastro.ivoacore.tap.TAPJob;
 import org.javastro.ivoacore.uws.JobManager;
 import org.javastro.ivoacore.uws.UWSException;
-import org.jspecify.annotations.NonNull;
 
 import java.net.URI;
 
@@ -21,7 +19,7 @@ public abstract class BaseTAPResource {
    @Inject
    JobManager jobmanager;
 
-   protected java.nio.file.@NonNull Path getResultPath(String jobid) throws UWSException {
+   protected java.nio.file.Path getResultPath(String jobid) throws UWSException {
       //FIXME this needs to be refactored to be generalize - it knows too much about the internal workings - particularly that the result is stored in local file - that is also being exposed in the results job structure at the moment
       String res = jobmanager.getJobResults(jobid).stream().filter(r -> r.getId().equals("result")).findFirst().orElseThrow(() -> new UWSException("No result with id 'result'")).getValue();
       java.nio.file.Path path = java.nio.file.Path.of(res);
