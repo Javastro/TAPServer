078180c1b1fc6ca8ad2aedc32eb69ba4aee9d89a
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-04-09 13:42:52 +0100

    fix test and extend with schema validation

--- src/test/java/org/javastro/ivoa/tap/VOSIResourceTest.java
+++ src/test/java/org/javastro/ivoa/tap/VOSIResourceTest.java
@@ -1,9 +1,14 @@
 package org.javastro.ivoa.tap;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.javastro.ivoa.schema.SchemaMap;
 import org.javastro.ivoa.schema.XMLValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -18,20 +23,19 @@ class VOSIResourceTest {
              .when().get("/VOSI/capabilities")
              .then()
              .statusCode(200)
-             .body("capabilities.capability.size()",equalTo(3))
+             .body("capabilities.capability.size()",equalTo(4))
              .extract().body().asString();
-       //FIXME do validation once the XMLValidator has been extended to allow non-registry schema
 
-//             XMLValidator xmlValidator = new XMLValidator(SchemaMap.getAllSchemaAsSources());
-//             xmlValidator.validate(new StreamSource(new StringReader(caps)));
-//             if(!xmlValidator.wasValid())
-//             {
-//
-//                xmlValidator.printErrors(System.err);
-//                System.err.println(caps);
-//
-//             }
-//             Assertions.assertTrue(xmlValidator.wasValid() ,"Capabilities validation failed");
+             XMLValidator xmlValidator = new XMLValidator(SchemaMap.getAllSchemaAsSources());
+             xmlValidator.validate(new StreamSource(new StringReader(caps)));
+             if(!xmlValidator.wasValid())
+             {
+
+                xmlValidator.printErrors(System.err);
+                System.err.println(caps);
+
+             }
+             Assertions.assertTrue(xmlValidator.wasValid() ,"Capabilities validation failed");
     }
 
 
@@ -46,12 +50,23 @@ class VOSIResourceTest {
 
    @Test
    void testTablesEndpoint() {
-      given()
+     String tables =  given()
             .when().get("/tables")
             .then()
             .statusCode(200)
             .log().body()
-      ;//TODO test return
+            .extract().body().asString();
+
+      XMLValidator xmlValidator = new XMLValidator(SchemaMap.getAllSchemaAsSources());
+      xmlValidator.validate(new StreamSource(new StringReader(tables)));
+      if(!xmlValidator.wasValid())
+      {
+
+         xmlValidator.printErrors(System.err);
+
+      }
+      Assertions.assertTrue(xmlValidator.wasValid() ,"tables validation failed");
+
    }
 
 }
