228c236c04e616285bd8b274d5f51666f6ffca9f
Stephen Lloyd <stephen.lloyd@manchester.ac.uk>
2026-06-19 14:18:58 +0100

    Upload unit test placeholder and an example VOTable for testing only added

---
+++ src/test/java/org/javastro/ivoa/tap/UploadTest.java
@@ -0,0 +1,13 @@
+package org.javastro.ivoa.tap;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class UploadTest {
+
+    @Test
+    public void testUpload() {
+
+    }
+}
